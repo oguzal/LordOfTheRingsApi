93f223ec49fa4d3b6a44eada30be0ef1e81fdbfd
oguzal <supertutorusa@hotmail.com>
2023-10-29 22:44:36 -0400

    Removed an unused class

--- LotrSDK/LotrSDK/Helpers/Request.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LotrSDK.Helpers
-{
-    public class Request
-    {
-        public int Offset { get; set; }
-        public int Limit { get; set; }
-        public int Page { get; set; }
-    }
-}
